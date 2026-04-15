bd1277c17cabc4d26f53bb31bb76d50ed4bb1c3b
smatplot0 <smatplot0@gmail.com>
2026-04-15 20:07:10 +0530

    added rain alert code

--- main.py
+++ main.py
@@ -1,1 +1,42 @@
 
+import requests
+from twilio.rest import Client
+import os
+
+OWM_Endpoint = "https://api.openweathermap.org/data/2.5/forecast"
+
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_SID")
+auth_token = os.environ.get("AUTH_TOKEN")
+
+weather_params = {
+    "lat": 28.7041,
+    "lon": 77.1025,
+    "appid": api_key,
+    "cnt": 4,
+}
+
+response = requests.get(OWM_Endpoint, params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+
+for hour_data in weather_data["list"]:
+    condition_code = hour_data["weather"][0]["id"]
+    if int(condition_code) < 700:
+        will_rain = True
+
+# TEMP TEST
+will_rain = True
+
+if will_rain:
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        body="It's going to rain today ☔",
+        from_="+17402453552",
+        to="+918745874773"
+    )
+    print(message.status)
+else:
+    print("No rain expected ☀️")
